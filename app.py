2ea169710e608513f639051bf86ef21172a0384b
Alice Xue <alicexue@dyn-160-39-135-138.dyn.columbia.edu>
2017-02-04 10:36:05 -0500

    final changes

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request, g, render_template, jsonify
-import sqlite3
-import os
+from flask_mail import Mail, Message
+import threading
+import schedule, time
 import data
-#import pandas as pd
 
 app = Flask(__name__)
 
-columnsthis = ['Email', 'Address']
-#df = pd.DataFrame(columns=columnsthis)
+mail = Mail(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'skm2159@columbia.edu'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mailing_list = []
 
 @app.route("/index.html", methods = ["GET", "POST"])
 @app.route("/index", methods = ["GET", "POST"])
@@ -18,11 +25,9 @@ def homepage():
         return render_template("index.html", facilities = results)
     else:
         results = data.get_data()
-        email = request.form['email_form']
-       # df2 = df.append({'Email': email, 'Address': "placeholder address"}, ignore_index=True)
-        #print df2
-        return render_template("index.html", facilities = results)
-    #return render_template("index.html", facilities = results, names = names, artists = artists, from_dates = from_dates, to_dates = to_dates, longitudes = longitudes, latitudes = latitudes, descriptions = descriptions, boroughs = boroughs)
+        email_address = request.form['email_form']
+        mailing_list.append(email_address)
+        return render_template("index.html", facilities = facilities)
 
 
 @app.route("/facilities.json", methods = ["GET"])
@@ -34,7 +39,22 @@ def facilities():
 def page_not_found(error):
     return "Sorry, this page was not found.", 404
 
+def send_emails(recipients):
+    message = Message("Test", recipients = mailing_list)
+    mail.send(message)
+
+def wait():
+    done = False
+    while not done:
+        schedule.run_pending()
+        time.sleep(3600)
+
 if __name__ == "__main__":
     app.debug = True
     app.secret_key="Don't store this on github"
     app.run(host = '0.0.0.0', port = 8080)
+
+    schedule.every().saturday.at("10:37").do(send_emails, args = [mailing_list])
+    email_thread = threading.Thread(target = wait())
+    threads.append(email_thread)
+    email_thread.start()
