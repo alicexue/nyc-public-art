98b50792bbd8a7a76c0b40ac657483d49105767f
Nellie Spektor <nellie.spektor@gmail.com>
2017-02-04 03:24:38 -0500

    working html

--- app.py
+++ app.py
@@ -2,30 +2,32 @@ from flask import Flask, request, g, render_template
 import sqlite3
 import os
 import data
-#import pandas as pd
+import pandas as pd
 
 app = Flask(__name__)
 
 columnsthis = ['Email', 'Address']
-#df = pd.DataFrame(columns=columnsthis)
+df = pd.DataFrame(columns=columnsthis)
 
 @app.route("/index.html", methods = ["GET", "POST"])
 @app.route("/index", methods = ["GET", "POST"])
 @app.route("/", methods = ["GET", "POST"])
 def homepage():
     if request.method == "GET":
-        facilities = data.get_data()
-        latitudes = []
-        longitudes = []
-        for facility in facilities:
-            latitudes = facility['lat']
-            longitudes = facility['lng']
-        return render_template("index.html", latitudes = latitudes, longitudes = longitudes)
+        # facilities = data.get_data()
+        # latitudes = []
+        # longitudes = []
+        # for facility in facilities:
+        #     latitudes = facility['lat']
+        #     longitudes = facility['lng']
+        # return render_template("index.html", latitudes = latitudes, longitudes = longitudes)
+        return render_template("index.html")
     else:
         email = request.form['email_form']
-       # df2 = df.append({'Email': email, 'Address': "placeholder address"}, ignore_index=True)
-        #print df2
-    return render_template("index.html", facilities = facilities)
+        df2 = df.append({'Email': email, 'Address': "placeholder address"}, ignore_index=True)
+        print df2
+    # return render_template("index.html", facilities = facilities)
+    return render_template("index.html")
 
 @app.errorhandler(404)
 def page_not_found(error):
