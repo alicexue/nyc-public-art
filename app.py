b7430a9742b4ea46293e55762dbfb74d219fd6b7
Alice Xue <alicexue@dyn-160-39-135-138.dyn.columbia.edu>
2017-02-04 02:56:21 -0500

    working now

--- app.py
+++ app.py
@@ -32,4 +32,7 @@ def page_not_found(error):
 	return "Sorry, this page was not found.", 404
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP', 'O.0.0.0'), port=int(os.getenv('PORT', 8080)))
+    app.debug = True
+    app.secret_key="Don't store this on github"
+    app.run(host = '0.0.0.0', port = 5000)
+    #app.run(host=os.getenv('IP', 'O.0.0.0'), port=int(os.getenv('PORT', 8080)))
